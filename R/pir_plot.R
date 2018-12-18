40c92f7c980c7e5d0043d7167553f0ab6ab00ab6
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-18 13:40:24 +0100

    pir_plot does something

--- R/pir_plot.R
+++ R/pir_plot.R
@@ -5,4 +5,14 @@
 #' @export
 pir_plot <- function(pir_out) {
 
+  df <- pir_out
+  first_col_index <- which(names(df) == "error_1")
+  df_long <- tidyr::gather(df, "error_index", "error_value", first_col_index:ncol(df))
+
+  ggplot2::ggplot(
+    data = df_long,
+    ggplot2::aes(x = tree, y = error_value, fill = inference_model)
+  ) + ggplot2::geom_violin() +
+    ggplot2::ggtitle("The error BEAST2 makes")
+
 }
--- tests/testthat/test-pir_plot.R
+++ tests/testthat/test-pir_plot.R
@@ -2,5 +2,5 @@ context("test-pir_plot")
 
 test_that("use", {
 
-  expect_silent(pir_plot(create_pir_run_test_output()))
+  expect_silent(pir_plot(pir_out = create_pir_run_test_output()))
 })
