6642177a6b8af58736a11f45102873866b7a4845
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-18 15:46:14 +0100

    EOD

--- tests/testthat/test-create_twin_tree.R
+++ tests/testthat/test-create_twin_tree.R
@@ -4,6 +4,10 @@ test_that("use", {
   tree <- ape::read.tree(text = "((A:1, B:1):1, C:2);")
   twin_tree <- create_twin_tree(tree)
   expect_equal(ape::Ntip(tree), ape::Ntip(twin_tree))
+
+  # dist_tree <- ape::dist.nodes(tree)
+  # dist_twin <- ape::dist.nodes(twin_tree)
+  # order(dist_tree[1, ]) == order(dist_twin[1, ])
 })
 
 test_that("use", {
