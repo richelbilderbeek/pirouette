dc684d580151b59b060bf605aceb31a0b74648f3
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-20 12:44:07 +0100

    Add example in 'create_alignment_params' doc

--- R/create_alignment_params.R
+++ R/create_alignment_params.R
@@ -3,6 +3,15 @@
 #' @param rng_seed the random number generator seed as used in the
 #'   simulation of an alignment
 #' @return a list of alignment parameters
+#' @examples
+#' n_base_pairs <- 4
+#' alignment_params <- create_alignment_params(
+#'    root_sequence = create_blocked_dna(length = n_base_pairs),
+#'    mutation_rate = 0.1
+#'  )
+#'  testit::assert("root_sequence" %in% names(alignment_params))
+#'  testit::assert("mutation_rate" %in% names(alignment_params))
+#'  testit::assert("rng_seed" %in% names(alignment_params))
 #' @export
 #' @author Richel J.C. Bilderbeek
 create_alignment_params <- function(
