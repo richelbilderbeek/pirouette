a133dbfdd78d2f04cc5b0a1c8932d48114482b77
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-07 14:00:18 +0100

    Fix test

--- tests/testthat/test-check_alignment_params.R
+++ tests/testthat/test-check_alignment_params.R
@@ -2,7 +2,7 @@ context("test-check_alignment_params")
 
 test_that("use", {
 
-  alignment_params <- create_alignment_params(
+  good_alignment_params <- create_alignment_params(
     root_sequence = "acgt",
     mutation_rate = 0.1
   )
@@ -10,28 +10,48 @@ test_that("use", {
   # OK
   expect_silent(
     check_alignment_params(
-      alignment_params
+      good_alignment_params
     )
   )
 
   # Wrong parameter names
+  alignment_params <- good_alignment_params
+  alignment_params$root_sequence <- NULL
   expect_error(
     check_alignment_params(
-      alignment_params[-1]
+      alignment_params
     ),
     "'root_sequence' must be an element of an 'alignment_params'"
   )
+
+  alignment_params <- good_alignment_params
+  alignment_params$mutation_rate <- NULL
   expect_error(
     check_alignment_params(
-      alignment_params[-2]
+      alignment_params
     ),
     "'mutation_rate' must be an element of an 'alignment_params'"
   )
+
+  alignment_params <- good_alignment_params
+  alignment_params$rng_seed <- NULL
   expect_error(
-    check_alignment_params(
-      alignment_params[-3]
-    ),
+    check_alignment_params(alignment_params),
     "'rng_seed' must be an element of an 'alignment_params'"
+  )
+
+  alignment_params <- good_alignment_params
+  alignment_params$site_model <- NULL
+  expect_error(
+    check_alignment_params(alignment_params),
+    "'site_model' must be an element of an 'alignment_params'"
+  )
+
+  alignment_params <- good_alignment_params
+  alignment_params$clock_model <- NULL
+  expect_error(
+    check_alignment_params(alignment_params),
+    "'clock_model' must be an element of an 'alignment_params'"
   )
 
   # Wrong parameter values
