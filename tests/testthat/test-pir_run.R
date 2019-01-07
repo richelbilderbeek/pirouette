6acf2b3db77bf389d7db25de026774c361553a22
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-07 12:58:51 +0100

    Can run 'generative' and 'most_evidence' in one go

--- tests/testthat/test-pir_run.R
+++ tests/testthat/test-pir_run.R
@@ -23,6 +23,7 @@ test_that("generative only", {
   expect_true("generative" %in% errors$inference_model)
 
   expect_true("inference_model_weight" %in% names(errors))
+  expect_true(is.na(errors$inference_model_weight))
   expect_true(!is.factor(errors$inference_model_weight))
 
   expect_true("site_model" %in% names(errors))
@@ -57,79 +58,16 @@ test_that("most_evidence only", {
     ),
     model_selections = "most_evidence"
   )
-  expect_true("tree" %in% names(errors))
-  expect_true(is.factor(errors$tree))
-  expect_true("true" %in% errors$tree)
-
-  expect_true("inference_model" %in% names(errors))
-  expect_true(is.factor(errors$inference_model))
-  expect_true("generative" %in% errors$inference_model)
-
-  expect_true("inference_model_weight" %in% names(errors))
-  expect_true(!is.factor(errors$inference_model_weight))
-
-  expect_true("site_model" %in% names(errors))
-  expect_true(is.factor(errors$site_model))
-  expect_true("JC69" %in% errors$site_model)
-
-  expect_true("clock_model" %in% names(errors))
-  expect_true(is.factor(errors$clock_model))
-  expect_true("strict" %in% errors$clock_model)
-
-  expect_true("tree_prior" %in% names(errors))
-  expect_true(is.factor(errors$tree_prior))
-  expect_true("birth_death" %in% errors$tree_prior)
-
-  expect_true("error_1" %in% names(errors))
-  expect_true(!is.factor(errors$error_1))
+  expect_true("most_evidence" %in% errors$inference_model)
+  expect_true(errors$inference_model_weight > 0.0)
 })
 
-test_that("also use inference model with most evidence", {
+test_that("generative and most_evidence", {
 
-  skip("Issue #10")
-  true_phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
-  errors <- pir_run(
-    true_phylogeny,
-    alignment_params = create_alignment_params(
-      root_sequence = "acgt",
-      mutation_rate = 0.01
-    ),
-    inference_params = create_inference_params(
-      mcmc = beautier::create_mcmc(chain_length = 2000, store_every = 1000)
-    )
-  )
-  expect_true("tree" %in% names(errors))
-  expect_true(is.factor(errors$tree))
-  expect_true("true" %in% errors$tree)
+  if (!beastier::is_on_travis()) return()
 
-  expect_true("inference_model" %in% names(errors))
-  expect_true(is.factor(errors$inference_model))
-  expect_true("generative" %in% errors$inference_model)
-
-  expect_true("inference_model_weight" %in% names(errors))
-  expect_true(!is.factor(errors$inference_model_weight))
-
-  expect_true("site_model" %in% names(errors))
-  expect_true(is.factor(errors$site_model))
-  expect_true("JC69" %in% errors$site_model)
-
-  expect_true("clock_model" %in% names(errors))
-  expect_true(is.factor(errors$clock_model))
-  expect_true("strict" %in% errors$clock_model)
-
-  expect_true("tree_prior" %in% names(errors))
-  expect_true(is.factor(errors$tree_prior))
-  expect_true("BD" %in% errors$tree_prior)
-
-  expect_true("error_1" %in% names(errors))
-  expect_true(!is.factor(errors$error_1))
-})
-
-test_that("pir_run_one on inference model with most evidence", {
-
-  skip("Issue #10")
   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
-  errors <- pirouette:::pir_run_one(
+  errors <- pir_run(
     phylogeny = phylogeny,
     alignment_params = create_alignment_params(
       root_sequence = "acgt",
@@ -138,32 +76,8 @@ test_that("pir_run_one on inference model with most evidence", {
     inference_params = create_inference_params(
       mcmc = beautier::create_mcmc(chain_length = 2000, store_every = 1000)
     ),
-    model_selection = "most_evidence"
-
+    model_selections = c("generative", "most_evidence")
   )
-  expect_true("tree" %in% names(errors))
-  expect_true(is.factor(errors$tree))
-  expect_true("true" %in% errors$tree)
-
-  expect_true("inference_model" %in% names(errors))
-  expect_true(is.factor(errors$inference_model))
-  expect_true("generative" %in% errors$inference_model)
-
-  expect_true("inference_model_weight" %in% names(errors))
-  expect_true(!is.factor(errors$inference_model_weight))
-
-  expect_true("site_model" %in% names(errors))
-  expect_true(is.factor(errors$site_model))
-  expect_true("JC69" %in% errors$site_model)
-
-  expect_true("clock_model" %in% names(errors))
-  expect_true(is.factor(errors$clock_model))
-  expect_true("strict" %in% errors$clock_model)
-
-  expect_true("tree_prior" %in% names(errors))
-  expect_true(is.factor(errors$tree_prior))
-  expect_true("BD" %in% errors$tree_prior)
-
-  expect_true("error_1" %in% names(errors))
-  expect_true(!is.factor(errors$error_1))
+  expect_true("most_evidence" %in% errors$inference_model)
+  expect_true(all(errors$inference_model_weight > 0.0))
 })
