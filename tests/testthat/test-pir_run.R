a43a6c3868edc42ababc148eabe2ead630e1a1aa
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-10 11:50:43 +0100

    Use simpler models to test

--- tests/testthat/test-pir_run.R
+++ tests/testthat/test-pir_run.R
@@ -61,9 +61,9 @@ test_that("most_evidence", {
     ),
     model_select_params = list(
       create_best_model_select_param(
-        site_models = beautier::create_site_models()[1],
-        clock_models = beautier::create_clock_models()[1],
-        tree_priors = beautier::create_tree_priors()[1]
+        site_models = beautier::create_site_models()[4],
+        clock_models = beautier::create_clock_models()[2],
+        tree_priors = beautier::create_tree_priors()[5]
       )
     ),
     inference_param = create_inference_param(
@@ -72,4 +72,34 @@ test_that("most_evidence", {
   )
   expect_true("most_evidence" %in% errors$inference_model)
   expect_true(all(errors$inference_model_weight > 0.0))
+})
+
+test_that("generative and most_evidence, generative not in most_evidence", {
+
+  if (!beastier::is_on_travis()) return()
+
+  phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
+  errors <- pir_run(
+    phylogeny = phylogeny,
+    alignment_params = create_alignment_params(
+      root_sequence = "acgt",
+      mutation_rate = 0.01
+    ),
+    model_select_params = list(
+      create_gen_model_select_param(
+        alignment_params = alignment_params
+      ),
+      create_best_model_select_param(
+        site_models = beautier::create_site_models()[4],
+        clock_models = beautier::create_clock_models()[2],
+        tree_priors = beautier::create_tree_priors()[5]
+      )
+    ),
+    inference_param = create_inference_param(
+      mcmc = beautier::create_mcmc(chain_length = 2000, store_every = 1000)
+    )
+  )
+  expect_true("most_evidence" %in% errors$inference_model)
+  expect_true(is.na(errors$inference_model_weight[1]))
+  expect_true(is.numeric(errors$inference_model_weight[2]))
 })
