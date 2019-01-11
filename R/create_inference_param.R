e3c034be99a0f9640632d1b770b233a5117279a9
Giappo <glaudanno@gmail.com>
2019-01-11 19:33:07 +0100

    added nltts functions and tests 2

--- R/create_inference_param.R
+++ R/create_inference_param.R
@@ -1,4 +1,7 @@
-#' Creates a set of parameters used in the Bayesian inference.
+#' Creates the parameters all BEAST2 runs share.
+#'
+#' These are for example, the MRCA prior, the MCMC, the RNG seed
+#' and the path to BEAST2.
 #' @inheritParams default_params_doc
 #' @param rng_seed the random number generator seed
 #'   used in the Bayesian inference.
@@ -6,26 +9,20 @@
 #'   R package, that calls the phylogenetic tool \code{BEAST2}.
 #' @author Richel J.C. Bilderbeek
 #' @export
-create_inference_params <- function(
-  site_model = beautier::create_jc69_site_model(),
-  clock_model = beautier::create_strict_clock_model(),
-  tree_prior = beautier::create_bd_tree_prior(),
+create_inference_param <- function(
   mrca_prior = NA,
   mcmc = beautier::create_mcmc(),
   rng_seed = NA,
   beast2_path = beastier::get_default_beast2_path(),
   verbose = FALSE
 ) {
-  inference_params <- list(
-    site_model = site_model,
-    clock_model = clock_model,
-    tree_prior = tree_prior,
+  inference_param <- list(
     mrca_prior = mrca_prior,
     mcmc = mcmc,
     rng_seed = rng_seed,
     beast2_path = beast2_path,
     verbose = verbose
   )
-  check_inference_params(inference_params) # nolint pirouette function
-  inference_params
+  check_inference_param(inference_param) # nolint pirouette function
+  inference_param
 }
