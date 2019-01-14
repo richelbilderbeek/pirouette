a0651a71e13c81d42ef2ec803da8315fc4d17b6d
Giappo <glaudanno@gmail.com>
2019-01-14 18:10:41 +0100

    only test pir_run

--- tests/testthat/test-get_model_selections.R
+++
@@ -1,8 +0,0 @@
-context("test-get_model_selections")
-
-test_that("use", {
-
-  expect_true("generative" %in% get_model_selections())
-  expect_true("most_evidence" %in% get_model_selections())
-  expect_false("nonsense" %in% get_model_selections())
-})
