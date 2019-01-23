22120c421c749c560f55acb15da7c92023b0e829
Giappo <glaudanno@gmail.com>
2019-01-23 16:29:39 +0100

    added list_model_select_params 6

--- R/list_model_select_params.R
+++ R/list_model_select_params.R
@@ -14,7 +14,6 @@ list_model_select_params <- function(model_select_params) {
   } else {
     listed_params <- model_select_params
   }
-  testit::assert(length(listed_params) == 1)
   testit::assert(is.list(listed_params) == 1)
   listed_params
 }
