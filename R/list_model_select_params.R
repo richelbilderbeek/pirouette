c8e487fc9009940b01cde721d4d5ee445256f7ad
Giappo <glaudanno@gmail.com>
2019-01-23 15:50:51 +0100

    added list_model_select_params 5

--- R/list_model_select_params.R
+++ R/list_model_select_params.R
@@ -2,7 +2,14 @@
 #' #' @inheritParams default_params_doc
 #' @author Giovanni Laudanno
 list_model_select_params <- function(model_select_params) {
-  if (length(model_select_params) > 1) {
+  expected_names <- names(
+    create_gen_model_select_param(
+      alignment_params = create_alignment_params(
+        mutation_rate = 0.01
+      )
+    )
+  )
+  if (expected_names[1] %in% names(model_select_params)) {
     listed_params <- list(model_select_params)
   } else {
     listed_params <- model_select_params
