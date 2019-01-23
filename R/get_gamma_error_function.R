521dbc69499791cacae3b7f27c3c0fc903747130
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-23 13:17:27 +0100

    Depend on phytools for gamma statistic, thanks @lintr-bot

--- R/get_gamma_error_function.R
+++ R/get_gamma_error_function.R
@@ -4,10 +4,14 @@
 get_gamma_error_function <- function() {
   gamma_error_function <- function(phylogeny, trees) {
     errors <- rep(NA, length(trees))
-    given_gamma <- phytools::gammatest(phytools::ltt(phylogeny, plot = FALSE, gamma = FALSE))$gamma
+    given_gamma <- phytools::gammatest(
+      phytools::ltt(phylogeny, plot = FALSE, gamma = FALSE)
+    )$gamma
     for (i in seq_along(trees)) {
       tree <- trees[[i]]
-      this_gamma <- phytools::gammatest(phytools::ltt(tree, plot = FALSE, gamma = FALSE))$gamma
+      this_gamma <- phytools::gammatest(
+        phytools::ltt(tree, plot = FALSE, gamma = FALSE)
+      )$gamma
       errors[i] <- abs(given_gamma - this_gamma)
     }
     errors
