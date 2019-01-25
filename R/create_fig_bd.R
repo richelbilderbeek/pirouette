51d29d28911d3d6d8ff40c6e676f03cf9fc11fb4
Giappo <glaudanno@gmail.com>
2019-01-25 17:13:34 +0100

    added create_figure_bd

--- R/create_fig_bd.R
+++ R/create_fig_bd.R
@@ -6,10 +6,8 @@
 create_fig_bd <- function() {
   phylogeny <- ape::read.tree(text = "((A:4, B:4):1, (C:4, D:4) :1);")
   i <- 31 #or 24
-  {
-    set.seed(i)
-    phylogeny <- DDD::dd_sim(pars = c(0.09, 0, Inf), age = 10, ddmodel = 1)$tes
-    figure_bd <- ggtree::ggtree(phylogeny) + ggtree::theme_tree2(); figure_bd
-  }
+  set.seed(i)
+  phylogeny <- DDD::dd_sim(pars = c(0.09, 0, Inf), age = 10, ddmodel = 1)$tes
+  figure_bd <- ggtree::ggtree(phylogeny) + ggtree::theme_tree2(); figure_bd
   figure_bd
 }
