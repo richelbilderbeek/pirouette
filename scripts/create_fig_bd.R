9cb47b0bbeb0555cf7f828e6c1219b92e7c40247
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-29 09:52:22 +0100

    Call create BD tree figure from script, progress #35 [skip ci]

--- scripts/create_fig_bd.R
+++ scripts/create_fig_bd.R
@@ -11,3 +11,26 @@ create_fig_bd <- function() {
   figure_bd <- ggtree::ggtree(phylogeny) + ggtree::theme_tree2(); figure_bd
   figure_bd
 }
+
+#' Create figure bd file
+#' @inheritParams default_params_doc
+#' @return the name of the file the plot is saved to
+#' @author Richel J.C. Bilderbeek, Giovanni Laudanno
+#' @export
+create_fig_bd_file <- function(
+  fig_bd_filename = "figure_bd.png"
+) {
+  # create figure
+  fig_bd <- create_fig_bd() # nolint internal function
+
+  # save output
+  ggplot2::ggsave(
+    filename = fig_bd_filename,
+    plot = fig_bd
+  )
+  fig_bd_filename
+}
+
+# Run
+library(pirouette)
+create_fig_bd_file()
