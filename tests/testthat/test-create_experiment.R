0fc6c13afac4c5de943bdaf9e9f0a34abfcde774
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-31 10:51:36 +0100

    'create_experiment' done, progress #69

--- tests/testthat/test-create_experiment.R
+++ tests/testthat/test-create_experiment.R
@@ -1,20 +1,26 @@
 context("test-create_experiment")
 
 test_that("use", {
-  skip("WIP #69")
+
   model_type <- "generative"
   run_if <- "always"
   do_measure_evidence <- FALSE
+  inference_model <- beautier::create_inference_model(
+    tipdates_filename = "something_special.csv"
+  )
+  beast2_options <- create_beast2_options(rng_seed = 42)
 
   experiment <- create_experiment(
     model_type = model_type,
     run_if = run_if,
     do_measure_evidence = do_measure_evidence,
-    inference_model = beautier::create_inference_model()
+    inference_model = inference_model,
+    beast2_options = beast2_options
   )
 
   expect_equal(model_type, experiment$model_type)
   expect_equal(run_if, experiment$run_if)
   expect_equal(do_measure_evidence, experiment$do_measure_evidence)
-  expect_equal(inference_model, inference_model$model_type)
+  expect_equal(inference_model, experiment$inference_model)
+  expect_equal(beast2_options, experiment$beast2_options)
 })
