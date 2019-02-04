5061116a6b5154d4c55eb07571c652e38eed8822
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-04 12:23:06 +0100

    Expose #78

--- R/pir_run.R
+++ R/pir_run.R
@@ -92,6 +92,11 @@ pir_run <- function(
 #'
 #' The phylogeny can be the true tree or its twin.
 #' @inheritParams default_params_doc
+#' @param evidence_epsilon relative error in estimating the
+#'   evidence (aka marginal likelihood).
+#'   TODO: put in \code{misc_params},
+#' @param evidence_filename filename to store the estimated
+#'   evidences (aka marginal likelihoods)
 #' @return a data frame with errors, with as many rows as model selection
 #'   parameter sets
 #' @export
@@ -103,7 +108,10 @@ pir_run_tree <- function(
   model_select_params = create_gen_model_select_param(alignment_params),
   inference_params = create_inference_params(),
   experiments = list(create_experiment()),
-  error_measure_params = create_error_measure_params()
+  error_measure_params = create_error_measure_params(),
+  evidence_epsilon = 1e-12,
+  evidence_filename = tempfile(fileext = ".csv")
+
 ) {
   testit::assert(tree_type %in% c("true", "twin"))
   # Simulate an alignment and save it to file (specified in alignment_params)
@@ -114,26 +122,37 @@ pir_run_tree <- function(
   testit::assert(file.exists(alignment_params$fasta_filename))
 
   # Estimate marginal likelihoods if needed
+  marg_liks <- NULL
   if (!beautier:::is_one_na(inference_params$rng_seed) &&
       inference_params$rng_seed == 314159265) { # nolint use new interface
     # STUB
     check_experiments(experiments) # nolint pirouette function
-  }
 
-  marg_liks <- NULL
-  for (model_select_param in model_select_params) {
-    if ("most_evidence" %in% model_select_param$type) {
-      marg_liks <- mcbette::est_marg_liks(
-        fasta_filename = alignment_params$fasta_filename,
-        site_models = model_select_param$site_models,
-        clock_models = model_select_param$clock_models,
-        tree_priors = model_select_param$tree_priors,
-        epsilon = model_select_param$epsilon,
-        verbose = model_select_param$verbose
-      )
-      utils::write.csv(
-        x = marg_liks, file = model_select_param$marg_lik_filename
-      )
+    marg_liks <- mcbette::est_marg_liks_from_models(
+      fasta_filename = fasta_filename,
+      inference_models = inference_models,
+      beast2_optionses = beast2_optionses,
+      epsilon = evidence_epsilon
+    )
+    utils::write.csv(
+      x = marg_liks, file = evidence_filename
+    )
+  } else {
+    # Use old interface
+    for (model_select_param in model_select_params) {
+      if ("most_evidence" %in% model_select_param$type) {
+        marg_liks <- mcbette::est_marg_liks(
+          fasta_filename = alignment_params$fasta_filename,
+          site_models = model_select_param$site_models,
+          clock_models = model_select_param$clock_models,
+          tree_priors = model_select_param$tree_priors,
+          epsilon = model_select_param$epsilon,
+          verbose = model_select_param$verbose
+        )
+        utils::write.csv(
+          x = marg_liks, file = model_select_param$marg_lik_filename
+        )
+      }
     }
   }
 
