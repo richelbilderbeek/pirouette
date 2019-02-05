a3437ac4f0e8f92b37893dffaa3774e1cd585717
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-05 11:33:40 +0100

    'select_experiments' can select best candidate

--- tests/testthat/test-select_experiments.R
+++ tests/testthat/test-select_experiments.R
@@ -10,16 +10,28 @@ test_that("use, always", {
 })
 
 test_that("use, most_evidence", {
-  skip("WIP, Issue 69, #69")
-  # Select all experiments with 'run_if' is 'always'
-  experiment_1 <- create_experiment()
-  experiment_2 <- create_experiment()
-  experiment_3 <- create_experiment()
-  experiment_1$run_if <- "best_candidate"
-  experiment_2$run_if <- "best_candidate"
-  experiment_3$run_if <- "best_candidate"
-  experiments <- list(experiment_1, experiment_2, experiment_3)
-  marg_liks <- data.frame(weights = c(0.1, 0.5, 0.4))
+  marg_liks <- create_test_marg_liks(
+    site_models = list(create_jc69_site_model()),
+    clock_models = list(create_strict_clock_model()),
+    tree_priors = list(create_yule_tree_prior(), create_bd_tree_prior())
+  )
+  marg_liks$weight <- c(0.9, 0.1) # in favor of Yule
+
+  experiment_yule <- create_experiment(
+    run_if = "best_candidate",
+    inference_model = create_inference_model(
+      tree_prior = create_yule_tree_prior()
+    )
+  )
+  experiment_bd <- create_experiment(
+    run_if = "best_candidate",
+    inference_model = create_inference_model(
+      tree_prior = create_bd_tree_prior()
+    )
+  )
+  experiments <- list(experiment_yule, experiment_bd)
   selected <- select_experiments(experiments, marg_liks)
+
   expect_equal(1, length(selected))
+  expect_equal("yule", selected[[1]]$inference_model$tree_prior$name)
 })
