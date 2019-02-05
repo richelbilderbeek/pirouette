e9593b7b75ab4150a47cdd29a5ddc9870c6616b9
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-05 10:24:16 +0100

    Thanks @lintr-bot

--- R/alignment_params_to_posterior_trees.R
+++ R/alignment_params_to_posterior_trees.R
@@ -54,7 +54,8 @@ alignment_params_to_posterior_trees <- function(# nolint indeed a long name
       beast2_output_log_filename = inference_model$beast2_output_log_filename,
       beast2_output_trees_filenames =
         inference_model$beast2_output_trees_filename,
-      beast2_output_state_filename = inference_model$beast2_output_state_filename,
+      beast2_output_state_filename =
+        inference_model$beast2_output_state_filename,
       beast2_path = inference_params$beast2_path
     )
   } else {
