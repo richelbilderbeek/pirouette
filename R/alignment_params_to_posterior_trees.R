e9e209a8be1330837bfff8b8023b82b48287255b
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-06 12:15:15 +0100

    'alignment_params_to_posterior_trees' removed old skool functionality

--- R/alignment_params_to_posterior_trees.R
+++ R/alignment_params_to_posterior_trees.R
@@ -20,6 +20,7 @@ alignment_params_to_posterior_trees <- function(# nolint indeed a long name
 
   # Use inference model (old skool) and experment (new skool)
   if (!beautier:::is_one_na(inference_model)) {
+    stop("Deprecated in 'alignment_params_to_posterior_trees' 1")
     check_old_skool_inference_model(inference_model) # nolint pirouette function
     testit::assert(beautier:::is_one_na(experiment))
   } else {
@@ -42,24 +43,7 @@ alignment_params_to_posterior_trees <- function(# nolint indeed a long name
 
   bbt_out <- NULL
   if (!beautier:::is_one_na(inference_model)) {
-    # Old skool
-    bbt_out <- babette::bbt_run(
-      fasta_filename = alignment_params$fasta_filename,
-      site_model = inference_model$site_model,
-      clock_model = inference_model$clock_model,
-      tree_prior = inference_model$tree_prior,
-      mrca_prior = inference_params$mrca_prior,
-      mcmc = inference_params$mcmc,
-      rng_seed = inference_params$rng_seed,
-      verbose = inference_params$verbose,
-      beast2_input_filename = inference_model$beast2_input_filename,
-      beast2_output_log_filename = inference_model$beast2_output_log_filename,
-      beast2_output_trees_filenames =
-        inference_model$beast2_output_trees_filename,
-      beast2_output_state_filename =
-        inference_model$beast2_output_state_filename,
-      beast2_path = inference_params$beast2_path
-    )
+    stop("Deprecated in 'alignment_params_to_posterior_trees' 2")
   } else {
     # New skool
     bbt_out <- babette::bbt_run_from_model(
