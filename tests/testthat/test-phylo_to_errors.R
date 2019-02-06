5f3e04cdc0ade40180f1059ff471dbe9f703e656
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-06 12:20:53 +0100

    Fixed tests for 'phylo_to_errors'

--- tests/testthat/test-phylo_to_errors.R
+++ tests/testthat/test-phylo_to_errors.R
@@ -2,43 +2,36 @@ context("test-phylo_to_errors")
 
 test_that("use", {
 
-  # ISSUE #42: TODO Richel
-  skip("For Richel: the check on fasta file existence will always fail")
+  if (!beastier::is_on_travis()) return()
+
+  phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
 
   # 'phylo_to_errors' expects an alignment file to be present
-  alignment_params <- create_alignment_params(
-    root_sequence = "acgt",
-    mutation_rate = 0.1,
-    site_model = beautier::create_jc69_site_model(),
-    clock_model = beautier::create_strict_clock_model(),
-    rng_seed = 0
-  )
+  alignment_params <- create_test_alignment_params()
 
   # Create the alignment
   sim_alignment_file(
-    fasta_filename = alignment_params$fasta_filename,
     phylogeny = phylogeny,
     alignment_params = alignment_params
   )
   testit::assert(file.exists(alignment_params$fasta_filename))
 
+  experiment <- create_experiment(
+    inference_model = create_inference_model(
+      mcmc = create_mcmc(chain_length = 2000, store_every = 1000)
+    )
+  )
+  experiments <- list(experiment)
+
   nltts <- phylo_to_errors(
     phylogeny = phylogeny,
     alignment_params = alignment_params,
-    inference_model = create_old_skool_inference_model(
-      site_model = beautier::create_jc69_site_model(),
-      clock_model = beautier::create_strict_clock_model(),
-      tree_prior = beautier::create_tree_prior_bd()
-    ),
+    inference_model = NA,
     inference_params = create_inference_params(
       mcmc = beautier::create_mcmc(chain_length = 2000)
     )
   )
 
-  expect_true(
-    length(nltts) > 0
-  )
-  expect_true(
-    all(nltts > 0) & all(nltts < 1)
-  )
+  expect_true(length(nltts) > 0)
+  expect_true(all(nltts > 0) & all(nltts < 1))
 })
