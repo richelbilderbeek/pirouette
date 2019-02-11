527e090aaf3ab21f0e0004f00b7374b89786b302
Giappo <glaudanno@gmail.com>
2019-02-11 15:26:30 +0100

    added to_twin_filename

---
+++ R/to_twin_filename.R
@@ -0,0 +1,12 @@
+#' Converts true tree filenames to twin tree filenames
+#' @inheritParams default_params_doc
+#' @return twin tree filename
+#' @author Giovanni Laudanno
+to_twin_filename <- function(
+  filename
+) {
+  x <- tools::file_path_sans_ext(filename)
+  y <- tools::file_ext(filename)
+  z <- paste0(x, "_twin.", y)
+  z
+}
