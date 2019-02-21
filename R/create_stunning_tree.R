72cc55aeabdf820d651a7b2de454fd7bd628c91e
Giappo <glaudanno@gmail.com>
2019-02-21 13:58:43 +0100

    fixed #129

--- R/create_stunning_tree.R
+++ R/create_stunning_tree.R
@@ -7,11 +7,12 @@
 #' @return the likelihood
 #' @noRd
 calc_likelihood_stunning <- function(
-    brts,
-    t_0 = 10,
-    n_taxa = 5,
-    n_0 = 2
-  ) {
+  brts,
+  t_0 = 10,
+  n_taxa = 5,
+  n_0 = 2,
+  precision = round(13 * n_taxa / 10)
+) {
   cond_1 <- (length(brts) != length(unique(brts)))
   cond_2 <- (length(brts) != (n_taxa - n_0))
   cond_3 <- (any(brts < 0))
@@ -22,11 +23,23 @@ calc_likelihood_stunning <- function(
     lambda <- (log(n_taxa) - log(n_0)) / t_0
     mu <- 0.0
     r <- 0.0 - (lambda - mu)
-    lik <- (1 - exp(2 * r * (t_0 - brts[1]))) *
-      (1 - exp(3 * r * (brts[1] - brts[2]))) *
-      (1 - exp(4 * r * (brts[2] - brts[3]))) *
-      (1 - exp(5 * r * brts[3]))
-    loglik <- log(lik)
+    brts2 <- c(t_0, brts, 0)
+    n_s <- seq(n_0, n_taxa)
+    x <- exp(n_s * r * (-diff(brts2)))
+    if (precision > 0) {
+      # Approximated likelihood: numerically more stable
+      exponents <- 1:precision
+      loglik <- sum(
+        outer(X = x, Y = exponents, FUN = function(x, y) -(x ^ y) / y)
+      )
+    }
+    if (precision == 0) {
+      # Full likelihood
+      loglik <- sum(log(1 - x))
+    }
+    if (precision < 0) {
+      stop("'precision' must be positive")
+    }
   }
   -loglik
 }
@@ -40,7 +53,8 @@ calc_likelihood_stunning <- function(
 create_stunning_tree <- function(
   t_0 = 10,
   n_taxa = 5,
-  n_0 = 2
+  n_0 = 2,
+  precision = round(13 * n_taxa / 10)
 ) {
   start_brts <- seq(from = 0, to = t_0, by = t_0 / (n_taxa - n_0 + 1))
   start_brts <- sort(start_brts[2:(length(start_brts) - 1)], decreasing = TRUE)
@@ -49,16 +63,23 @@ create_stunning_tree <- function(
     fn = calc_likelihood_stunning,
     t_0 = t_0,
     n_taxa = n_taxa,
-    n_0 = n_0
+    n_0 = n_0,
+    precision = precision
   )
   brts <- sort(c(rep(t_0, n_0), x$par), decreasing = TRUE)
-  brts <- signif(brts, digits = 2)
+  brts <- signif(
+    brts,
+    digits = round(log(2 + n_taxa / 2))
+  )
   l_table <- matrix(NA, nrow = n_taxa, ncol = 4)
   l_table[, 4] <- -1
-  l_table[, 3] <- c(1, -2, 3, -4, 5)
-  l_table[, 2] <- c(0, 1, 1, -2, 3)
+  l_table[, 3] <- seq(from = 1, to = n_taxa) *
+    cos(pi * seq(from = 0, to = n_taxa - 1))
+  l_table[, 2] <- seq(from = 0, to = n_taxa - 1) *
+    cos(pi * seq(from = 1, to = n_taxa))
   l_table[, 1] <- brts
   tree <- DDD::L2phylo(L = l_table)
-  tree$tip.label <- LETTERS[1:n_taxa] # nolint ape::phylo does not use snake_case
+  LETTERS702 <- c(LETTERS, sapply(LETTERS, function(x) paste0(x, LETTERS))) # nolint
+  tree$tip.label <- LETTERS702[1:n_taxa] # nolint ape::phylo does not use snake_case
   tree
 }
