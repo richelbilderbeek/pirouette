71498e3cfaee8dc4c5be4638968c00899e546bc2
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-25 11:33:51 +0100

    Expect error in 'save_errors_to_file' test

--- tests/testthat/test-save_errors_to_file.R
+++ tests/testthat/test-save_errors_to_file.R
@@ -1,6 +1,11 @@
 context("test-save_errors_to_file")
 
 test_that("df incorrect", {
-  pir_params <- create_test_pir_params()
-  save_errors_to_file(ape::rcoal(4), pir_params = pir_params)
+  expect_error(
+    save_errors_to_file(
+      ape::rcoal(4),
+      pir_params = create_test_pir_params()
+    ),
+    "Saving errors to file failed"
+  )
 })
