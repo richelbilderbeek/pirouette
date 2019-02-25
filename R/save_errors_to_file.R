d848280e96d61f109d4c3da6db532ee3fc1e4240
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-25 10:32:41 +0100

    'save_errors_to_file' more explicit in error handling

--- R/save_errors_to_file.R
+++ R/save_errors_to_file.R
@@ -2,17 +2,25 @@
 #' @param df output dataframe from pir_run
 #' @inheritParams default_params_doc
 #' @return nothing
-#' @author Giovanni Laudanno
+#' @author Giovanni Laudanno, Richel J.C. Bilderbeek
 #' @noRd
 save_errors_to_file <- function(
   df,
   pir_params
-) {2
+) {
   check_pir_params(pir_params) # nolint pirouette function
   filename <- pir_params$error_measure_params$errors_filename
-  utils::write.csv(
-    x = df,
-    file = filename
+  tryCatch(
+    utils::write.csv(
+      x = df,
+      file = filename
+    ),
+    error = function(e) {
+      stop(
+        "Saving errors to file failed. \n",
+        "Filename: ", filename, "\n",
+        "Error: ", e$message
+      )
+    }
   )
-  invisible()
 }
