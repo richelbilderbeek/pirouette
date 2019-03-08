fc6ad05257850767f39dcac9486d2d524de99c1c
Giappo <glaudanno@gmail.com>
2019-03-08 16:32:59 +0100

    improved codecov

--- tests/testthat/test-check_experiments.R
+++ tests/testthat/test-check_experiments.R
@@ -5,6 +5,16 @@ test_that("use", {
   expect_silent(check_experiments(experiments = list(create_experiment())))
 })
 
+test_that("each element in the list must be a proper experiment", {
+
+  experiment_1 <- create_experiment()
+  experiment_2 <- "nonsense"
+  experiments <- list(experiment_1, experiment_2)
+  expect_error(
+    check_experiments(experiments)
+  )
+})
+
 test_that("must have same MCMC chain length", {
 
   experiment_1 <- create_experiment()
@@ -15,5 +25,16 @@ test_that("must have same MCMC chain length", {
   expect_error(
     check_experiments(experiments),
     "All MCMCs in the experiments must be identical"
+  )
+})
+
+test_that("no more than one generative", {
+
+  experiment_1 <- create_experiment()
+  experiment_2 <- create_experiment()
+  experiments <- list(experiment_1, experiment_2)
+  expect_error(
+    check_experiments(experiments),
+    "Specifying more than one 'generative' model experiment is redundant."
   )
 })
