75e73d4f4fcee6dfea197f294a9bf94704bd8bfb
Giappo <glaudanno@gmail.com>
2019-03-08 17:33:35 +0100

    small change

--- R/zzz.R
+++ R/zzz.R
@@ -2,14 +2,7 @@
 
   suppressPackageStartupMessages(
     lapply(
-      c(
-        "beautier",
-        "beastier",
-        "tracerer",
-        "mauricer",
-        "babette",
-        "mcbette"
-      ),
+      c("beautier", "beastier", "tracerer", "mauricer", "babette", "mcbette"),
       library,
       character.only = TRUE,
       warn.conflicts = FALSE
