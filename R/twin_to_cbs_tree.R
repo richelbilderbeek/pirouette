8a85325547b63240043b368e808e899da21c4e31
richelbilderbeek <richel@richelbilderbeek.nl>
2019-03-15 13:08:54 +0100

    Fix doc

--- R/twin_to_cbs_tree.R
+++ R/twin_to_cbs_tree.R
@@ -6,7 +6,7 @@
 #' @export
 twin_to_cbs_tree <- function(
   phylogeny = ape::read.tree(text = "((A:1, B:1):1, C:2);"),
-  winning_params = create_twinning_params()
+  twinning_params = create_twinning_params()
 ) {
   # STUB
   twin_to_yule_tree(
