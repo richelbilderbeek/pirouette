b9e1d328bdf19c6d6871bcc2798e562b180e1bc5
richelbilderbeek <richel@richelbilderbeek.nl>
2019-03-19 17:02:07 +0100

    'pir_table' works well on test run_experiments

--- R/pir_table.R
+++ R/pir_table.R
@@ -9,8 +9,14 @@
 pir_table <- function(run_experiments) {
   check_run_experiments(run_experiments) # nolint pirouette function
 
-  # Put inference models and errors a data frame
-  n_rows <- length(run_experiments)
+  # Count the number of rows needed
+  n_rows <- 0
+  for (run_experiment in run_experiments) {
+    if (!is_one_na(run_experiment$true_result)) n_rows <- n_rows + 1
+    if (!is_one_na(run_experiment$twin_result)) n_rows <- n_rows + 1
+  }
+
+  # Prepare data frame
   df <- data.frame(
     tree = rep(NA, n_rows),
     inference_model = rep(NA, n_rows),
@@ -19,9 +25,24 @@ pir_table <- function(run_experiments) {
     clock_model = rep(NA, n_rows),
     tree_prior = rep(NA, n_rows)
   )
+
+  # Find the highest number of errors measured
   max_n_errors <- 0
   for (run_experiment in run_experiments) {
-    max_n_errors <- max(length(run_experiment$errors), max_n_errors)
+    if (!is_one_na(run_experiment$true_result) &&
+      !is_one_na(run_experiment$true_result$errors)
+    ) {
+      max_n_errors <- max(
+        length(run_experiment$true_result$errors), max_n_errors
+      )
+    }
+    if (!is_one_na(run_experiment$twin_result) &&
+      !is_one_na(run_experiment$twin_result$errors)
+    ) {
+      max_n_errors <- max(
+        length(run_experiment$twin_result$errors), max_n_errors
+      )
+    }
   }
 
   error_col_names <- paste0("error_", seq(1, max_n_errors))
@@ -31,17 +52,40 @@ pir_table <- function(run_experiments) {
     run_experiment <- run_experiments[[i]]
     check_run_experiment(run_experiment) # nolint pirouette function
 
-    errors <- run_experiment$errors
-    df$tree[i] <- "?" #run_experiment$inference_conditions$model_type
-    df$inference_model[i] <- run_experiment$inference_conditions$model_type
-    df$inference_model_weight[i] <- NA
-    df$site_model[i] <- run_experiment$inference_model$site_model$name
-    df$clock_model[i] <- run_experiment$inference_model$clock_model$name
-    df$tree_prior[i] <- run_experiment$inference_model$tree_prior$name
-    from_col_idx <- which(colnames(df) == "error_1")
-    df[i, from_col_idx:ncol(df)] <- errors
-    df$log_evidence[i] <- run_experiment$log_evidence
-    df$weight[i] <- run_experiment$weight
+    # True
+    if (!is_one_na(run_experiment$true_result)) {
+      result <- run_experiment$true_result
+      errors <- result$errors
+      df$tree[i] <- "true"
+      df$inference_model[i] <- run_experiment$inference_conditions$model_type
+      df$inference_model_weight[i] <- result$weight
+      df$site_model[i] <- run_experiment$inference_model$site_model$name
+      df$clock_model[i] <- run_experiment$inference_model$clock_model$name
+      df$tree_prior[i] <- run_experiment$inference_model$tree_prior$name
+      from_col_idx <- which(colnames(df) == "error_1")
+      to_col_idx <- from_col_idx - 1 + length(errors)
+      df[i, from_col_idx:to_col_idx] <- errors
+      #df$log_evidence[i] <- result$log_evidence
+      #df$weight[i] <- result$weight
+      i <- i + 1
+    }
+    # Twin
+    if (!is_one_na(run_experiment$twin_result)) {
+      result <- run_experiment$twin_result
+      errors <- result$errors
+      df$tree[i] <- "twin"
+      df$inference_model[i] <- run_experiment$inference_conditions$model_type
+      df$inference_model_weight[i] <- result$weight
+      df$site_model[i] <- run_experiment$inference_model$site_model$name
+      df$clock_model[i] <- run_experiment$inference_model$clock_model$name
+      df$tree_prior[i] <- run_experiment$inference_model$tree_prior$name
+      from_col_idx <- which(colnames(df) == "error_1")
+      to_col_idx <- from_col_idx - 1 + length(errors)
+      df[i, from_col_idx:to_col_idx] <- errors
+      #df$log_evidence[i] <- result$log_evidence
+      #df$weight[i] <- result$weight
+      i <- i + 1
+    }
   }
 
   df$tree <- as.factor(df$tree)
