97a9b79628ff12c682afe7b2010383fcfb43c8a1
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-08 11:21:12 +0200

    Expose #257

--- tests/testthat/test-count_n_mutations.R
+++ tests/testthat/test-count_n_mutations.R
@@ -111,3 +111,21 @@ test_that("abuse", {
     "'root_sequence' must have the same length as each taxon's sequence length"
   )
 })
+
+test_that("Bug #257", {
+  skip("#257")
+  true_tree <- ape::read.tree(text = "((A:1, B:1):2, C:3);")
+  root_sequence <- create_blocked_dna(1000)
+  true_alignment <- sim_alignment(
+    phylogeny = true_tree,
+    alignment_params = create_test_alignment_params(
+      root_sequence = root_sequence
+    )
+  )
+  image(true_alignment)
+  expect_silent(
+    count_n_mutations(
+      alignment = true_alignment, root_sequence = root_sequence
+    )
+  )
+})
