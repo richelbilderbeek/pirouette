d8e4d76b33f9f1ca4883cf338131dbb0d162fcac
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-08 11:22:14 +0200

    Add extra test for 'create_twin_alignment'

--- tests/testthat/test-create_twin_alignment.R
+++ tests/testthat/test-create_twin_alignment.R
@@ -26,5 +26,32 @@ test_that("use, twin has more info", {
       alignment = twin_alignment, root_sequence = root_sequence
     )
   )
+})
 
+test_that("use, twin has less info", {
+  skip("#255")
+  true_tree <- ape::read.tree(text = "((A:2, B:2):1, C:3);")
+  twin_tree <- ape::read.tree(text = "((A:1, B:1):2, C:3);")
+  root_sequence <- create_blocked_dna(1000)
+  true_alignment <- sim_alignment(
+    phylogeny = true_tree,
+    alignment_params = create_test_alignment_params(
+      root_sequence = root_sequence
+    )
+  )
+  image(true_alignment)
+
+  twin_alignment <- create_twin_alignment(
+    true_phylogeny = true_tree,
+    twin_phylogeny = twin_tree,
+    true_alignment = true_alignment
+  )
+  expect_equal(
+    count_n_mutations(
+      alignment = true_alignment, root_sequence = root_sequence
+    ),
+    count_n_mutations(
+      alignment = twin_alignment, root_sequence = root_sequence
+    )
+  )
 })
