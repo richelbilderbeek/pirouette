2a452956ea4aeec47c0203fad3141a47a7d1883e
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-11 07:43:37 +0200

    Fix test

--- tests/testthat/test-pir_plot_from_file.R
+++ tests/testthat/test-pir_plot_from_file.R
@@ -4,7 +4,7 @@ test_that("use", {
   url <- "https://raw.githubusercontent.com/richelbilderbeek/pirouette_example_6/master/example_6/errors.csv" # nolint indeed a long URL
   utils::download.file(url = url, destfile = pir_out_filename)
 
-  expect_silent(
-    pir_plot_from_file(pir_out_filename)
-  )
+  plot <- pir_plot_from_file(pir_out_filename)
+
+  expect_equal(class(plot), c("gg", "ggplot"))
 })
