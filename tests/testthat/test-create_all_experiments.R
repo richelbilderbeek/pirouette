6c182053d09d1161352b6a5664b265ba7db65326
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-13 11:21:32 +0200

    Expose #278

--- tests/testthat/test-create_all_experiments.R
+++ tests/testthat/test-create_all_experiments.R
@@ -1,8 +1,8 @@
 context("test-create_all_experiments")
 
 test_that("use", {
+  # if (rappdirs::app_dir()$os == "win")  return() # nolint I think I can comment this out
 
-  if (rappdirs::app_dir()$os == "win") return()
   all_experiments <- create_all_experiments()
   for (i in seq_along(all_experiments)) {
     experiment <- all_experiments[[i]]
@@ -11,8 +11,7 @@ test_that("use", {
 })
 
 test_that("it can exclude an experiment", {
-
-  if (rappdirs::app_dir()$os == "win")  return()
+  # if (rappdirs::app_dir()$os == "win")  return() # nolint I think I can comment this out
 
   all_experiments <- create_all_experiments(
     exclude_model = create_inference_model(
@@ -23,4 +22,32 @@ test_that("it can exclude an experiment", {
     )
   )
   expect_true(length(all_experiments) == (length(create_all_experiments()) - 1))
+})
+
+
+
+test_that("should exclude one model, code from article", {
+
+  skip("#278")
+  # if (rappdirs::app_dir()$os == "win")  return() # nolint I think I can comment this out
+
+  generative_experiment <- create_experiment(
+    inference_conditions = create_inference_conditions(
+      model_type = "generative",
+      run_if = "always"
+    ),
+    inference_model = create_inference_model(
+      tree_prior = create_yule_tree_prior(),
+      clock_model = create_strict_clock_model(),
+      site_model = create_jc69_site_model()
+    )
+  )
+
+  candidate_experiments <- create_all_experiments(
+    exclude_model = generative_experiment$inference_model
+  )
+  expect_equal(
+    length(candidate_experiments),
+    length(create_all_experiments()) - 1
+  )
 })
