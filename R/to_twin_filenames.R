428fd06318cc5c650382bae7d6d151b7f7a91763
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-29 16:39:57 +0200

    Thanks @lintr-bot

--- R/to_twin_filenames.R
+++ R/to_twin_filenames.R
@@ -3,7 +3,7 @@
 #' @export
 to_twin_filenames <- function(filenames) {
   for (i in seq_along(filenames)) {
-    filenames[i] <- to_twin_filename(filenames[i])
+    filenames[i] <- to_twin_filename(filenames[i]) # nolint pirouette function
   }
   filenames
 }
