4ac635388ebc5836d3843254dd8b0adc2a345f44
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-29 10:26:07 +0200

    Add 'errorses_to_data_frame', simplified 'pir_run_(true|twin)_tree'

--- R/pir_run_twin_tree.R
+++ R/pir_run_twin_tree.R
@@ -65,77 +65,8 @@ pir_run_twin_tree <- function(
       file = errors_filename
     )
   }
-  testit::assert(length(errorses) > 0)
-  testit::assert(length(experiments) == length(errorses))
-  if (length(errorses) > 1) {
-    if (length(errorses[[1]]) != length(errorses[[2]])) {
-      warning(
-        "Lengths between errorses differ (", length(errorses[[1]]),
-        " vs ", length(errorses[[2]]), "). This is related to #99. ",
-        "Fixing this by shortening the longer errorses"
-      )
-      shortest <- min(length(errorses[[1]]), length(errorses[[2]]))
-      errorses[[1]] <- errorses[[1]][1:shortest]
-      errorses[[2]] <- errorses[[2]][1:shortest]
-    }
-    testit::assert(length(errorses[[1]]) == length(errorses[[2]]))
-  }
-
-  # Put inference models and errors a data frame
-  n_rows <- length(experiments)
-  df <- data.frame(
-    tree = rep(NA, n_rows),
-    inference_model = rep(NA, n_rows),
-    inference_model_weight = rep(NA, n_rows),
-    site_model = rep(NA, n_rows),
-    clock_model = rep(NA, n_rows),
-    tree_prior = rep(NA, n_rows)
-  )
-
-  error_col_names <- paste0("error_", seq(1, length(errorses[[1]])))
-  df[, error_col_names] <- NA
-
-  for (i in seq_along(experiments)) {
-    experiment <- experiments[[i]]
-    nltts <- errorses[[i]]
-
-    df$tree[i] <- "twin"
-    check_experiment(experiment) # nolint pirouette function
-    df$inference_model[i] <- experiment$inference_conditions$model_type
-    df$inference_model_weight[i] <- NA
-    df$site_model[i] <- experiment$inference_model$site_model$name
-    df$clock_model[i] <- experiment$inference_model$clock_model$name
-    df$tree_prior[i] <- experiment$inference_model$tree_prior$name
-    from_col_idx <- which(colnames(df) == "error_1")
-    df[i, from_col_idx:ncol(df)] <- nltts
-  }
-
-  # Add evidence (marginal likelihoods) in columns
-  if (!is.null(marg_liks)) {
-    for (i in seq_along(experiments)) {
-      experiment <- experiments[[i]]
-      marg_liks_row <- which(
-        marg_liks$site_model_name ==
-          experiment$inference_model$site_model$name &
-        marg_liks$clock_model_name ==
-          experiment$inference_model$clock_model$name &
-        marg_liks$tree_prior_name ==
-          experiment$inference_model$tree_prior$name
-      )
-      # if there is no row, 'which' returns a zero-length vector
-      # Happens when the generative model is not part of the models
-      # under selection
-      if (length(marg_liks_row) != 0) {
-        df$inference_model_weight[i] <- marg_liks$weight[marg_liks_row]
-      }
-    }
-  }
-
+  df <- errorses_to_data_frame(errorses)
+  df$tree <- "twin"
   df$tree <- as.factor(df$tree)
-  df$inference_model <- as.factor(df$inference_model)
-  df$site_model <- as.factor(df$site_model)
-  df$clock_model <- as.factor(df$clock_model)
-  df$tree_prior <- as.factor(df$tree_prior)
-
   df
 }
