bd0682006749a5d324977ab1aa1796d6017a78cc
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-30 08:58:43 +0200

    Thanks @lintr-bot

--- R/get_experiments_filenames.R
+++ R/get_experiments_filenames.R
@@ -11,8 +11,8 @@ get_experiments_filenames <- function(experiments) {
   filenames <- rep(NA, n_filenames_per_experiment * length(experiments))
   for (i in seq_along(experiments)) {
     filenames_here <- get_experiment_filenames(experiments[[i]]) # nolint pirouette function
-
-    from_index <- 1 + ((i - 1) * n_filenames_per_experiment)
+    i_minus_one <- i - 1
+    from_index <- 1 + (i_minus_one * n_filenames_per_experiment)
     to_index <- from_index + n_filenames_per_experiment - 1
     filenames[from_index:to_index] <- filenames_here
   }
