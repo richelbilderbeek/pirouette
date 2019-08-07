3f6752e4cfbc7e10eabff30223e2a47c04f9000f
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-07 10:55:51 +0200

    Remove obsolete 'to_fasta_text'

--- R/to_fasta_text.R
+++
@@ -1,9 +0,0 @@
-#' Show a DNA alignment as text in FASTA format
-#' @export
-to_fasta_text <- function(
-  alignment,
-  alignment_file = tempfile(pattern = "alignment_", fileext = ".fasta")
-) {
-  ape::write.FASTA(x = alignment, file = alignment_file)
-  readLines(alignment_file)
-}
