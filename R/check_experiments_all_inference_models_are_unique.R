f9e5739d789898782a42039e1e74c7aae0a3d5f5
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-19 14:33:15 +0200

    Fix doc

--- R/check_experiments_all_inference_models_are_unique.R
+++ R/check_experiments_all_inference_models_are_unique.R
@@ -1,33 +1,5 @@
-#' Checks if the argument is a list of one or more \link{pirouette} experiments.
-#'
-#' Will \link{stop} if not.
-#' A valid \link{pirouette} experiment
-#' can be created by \link{create_experiment}.
-#' @inheritParams default_params_doc
-#' @return nothing. Will \link{stop} if not
-#' @seealso Use \link{check_experiment} to check if an object
-#'   is one valid experiment
-#' @examples
-#' library(testthat)
-#'
-#' expect_silent(check_experiments(list(create_test_experiment())))
-#' expect_error(check_experiments(create_test_experiment()))
-#' expect_error(check_experiments("nonsense"))
-#' expect_error(check_experiments(NA))
-#' expect_error(check_experiments(NULL))
-#'
-#' if (rappdirs::app_dir()$os != "win") {
-#'   expect_silent(
-#'     check_experiments(
-#'       list(
-#'         create_test_experiment(),
-#'         create_test_cand_experiment()
-#'       )
-#'     )
-#'   )
-#' }
 #' @author Richèl J.C. Bilderbeek
-#' @export
+#' @noRd
 check_experiments_all_inference_models_are_unique <- function(
   experiments
 ) {
