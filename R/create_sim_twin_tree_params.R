12674cb24a087e86f3b55e9ad4d4cc553fd4ec72
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-14 13:18:50 +0200

    Start of 'create_sim_twin_tree_params'

---
+++ R/create_sim_twin_tree_params.R
@@ -0,0 +1,5 @@
+#' Create the parameters how to simulate a twin tree
+#' @export
+create_sim_twin_tree_params <- function() {
+
+}
---
+++ tests/testthat/test-create_sim_twin_tree_params.R
@@ -0,0 +1,3 @@
+test_that("use", {
+  expect_silent(create_sim_twin_tree_params())
+})
