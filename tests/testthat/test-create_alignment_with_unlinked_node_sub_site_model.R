4e58d8cfcbc86dde66d232005c7635bb2712a6c1
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-14 11:59:36 +0200

    'create_alignment_with_(un)?linked_node_sub_site_model' produces alignment of always 1k length, expose #338

--- tests/testthat/test-create_alignment_with_unlinked_node_sub_site_model.R
+++ tests/testthat/test-create_alignment_with_unlinked_node_sub_site_model.R
@@ -1,12 +1,15 @@
 test_that("number of nucleotides must match", {
   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
   alignment_params <- create_alignment_params(
-    site_model = "unlinked_node_sub"
+    site_model = "unlinked_node_sub",
+    root_sequence = "acgt"
   )
   alignment <- create_alignment_with_unlinked_node_sub_site_model(
     phylogeny = phylogeny,
     alignment_params = alignment_params
   )
   expect_equal(nrow(alignment), ape::Ntip(phylogeny))
+
+  skip("Issue #338")
   expect_equal(ncol(alignment), nchar(alignment_params$root_sequence))
 })
