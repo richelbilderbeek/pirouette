670cab3c56f6ef0e246d9c9e4b29ca270164f5c4
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-29 13:22:06 +0100

    Fix doc

--- R/create_copy_twin_tree_from_true_function.R
+++ R/create_copy_twin_tree_from_true_function.R
@@ -1,6 +1,22 @@
 #' Create a function that can simulate the twin tree from the true tree,
 #' by just copying the true tree
 #' @return a function
+#' @seealso
+#' Use \link{create_sim_yule_twin_tree_function} to get a function to
+#' produce a Yule tree.
+#' Use \link{create_sim_bd_twin_tree_function} to get a function to
+#' produce a Birth-Death tree.
+#' @examples
+#' library(testthat)
+#'
+#' f <- create_copy_twin_tree_from_true_function()
+#' phylo_in <- ape::read.tree(text = "((A:1, B:1):1, C:2);")
+#' phylo_out <- f(phylo_in)
+#' expect_equal(
+#'   ape::branching.times(phylo_in),
+#'   ape::branching.times(phylo_out)
+#' )
+#' @author Richèl J.C. Bilderbeek
 #' @export
 create_copy_twin_tree_from_true_function <- function() {
   function(phylogeny) phylogeny
--- R/create_sim_yule_twin_tree_function.R
+++ R/create_sim_yule_twin_tree_function.R
@@ -2,13 +2,21 @@
 #' using the Yule speciation model
 #' @inheritParams default_params_doc
 #' @return a function
+#' @seealso
+#' Use \link{create_sim_bd_twin_tree_function} to get a function to
+#' produce a Birth-Death tree.
+#' Use \link{create_copy_twin_tree_from_true_function} to get a
+#' function to simply copy the tree
 #' @examples
+#' library(testthat)
+#'
 #' f <- create_sim_yule_twin_tree_function()
 #' phylo_in <- ape::read.tree(text = "((A:1, B:1):1, C:2);")
 #' phylo_out <- f(phylo_in)
 #' expect_true(
 #'   !all(ape::branching.times(phylo_in) == ape::branching.times(phylo_out))
 #' )
+#' @author Richèl J.C. Bilderbeek
 #' @export
 create_sim_yule_twin_tree_function <- function(
   seed = 0,
