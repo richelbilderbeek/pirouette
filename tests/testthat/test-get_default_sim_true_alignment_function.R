756ac8262fc0bdc121875001b842aeb2fb996930
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-30 11:17:57 +0100

    Be explicit in which alignment to simulate, as the function signature differs

--- tests/testthat/test-get_default_sim_true_alignment_function.R
+++ tests/testthat/test-get_default_sim_true_alignment_function.R
@@ -1,5 +1,5 @@
 test_that("use", {
-  f <- get_default_sim_alignment_function()
+  f <- get_default_sim_true_alignment_function()
   expect_true(is.function(f))
   alignment <- f(ape::rcoal(3))
   expect_equal(class(alignment), "DNAbin")
