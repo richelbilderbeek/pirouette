337350abeba96e477b5dae0a78d94112bfe0817e
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-31 13:37:43 +0100

    'sim_true_alignment' does a good job

---
+++ R/sim_true_alignment.R
@@ -0,0 +1,42 @@
+#' Simulate the true alignment from the true phylogeny
+#'
+#' @inheritParams default_params_doc
+#' @return an alignment of type \link[ape]{DNAbin}
+#' @seealso Use \link{sim_true_alignment_file} to save the simulated alignment
+#'   directly to a file
+#' @examples
+#' library(testthat)
+#'
+#' # Create the phylogeny to simulate the alignment on
+#' n_taxa <- 5
+#' phylogeny <- ape::rcoal(n_taxa)
+#'
+#' # Use default settings to create the alignment
+#' alignment_params <- create_alignment_params()
+#'
+#' # Simulate the alignment
+#' alignment <- sim_true_alignment(
+#'    phylogeny = phylogeny,
+#'    alignment_params = alignment_params,
+#'  )
+#'
+#' expect_equal(class(alignment), "DNAbin")
+#' expect_equal(nrow(alignment), n_taxa)
+#' expect_equal(ncol(alignment), nchar(alignment_params$root_sequence))
+#' @author Richèl J.C. Bilderbeek
+#' @export
+sim_true_alignment <- function(
+  phylogeny,
+  alignment_params = create_alignment_params(),
+  verbose = FALSE
+) {
+  beautier::check_phylogeny(phylogeny)
+  pirouette::check_alignment_params(alignment_params)
+  pirouette::check_reconstructed_phylogeny(phylogeny)
+  testit::assert(beautier::is_one_bool(verbose))
+
+  set.seed(alignment_params$rng_seed)
+  alignment <- alignment_params$sim_true_alignment_function(phylogeny)
+  check_alignment(alignment)
+  alignment
+}
---
+++ tests/testthat/test-sim_true_alignment.R
@@ -0,0 +1,58 @@
+test_that("minimal use", {
+
+  expect_silent(
+    sim_true_alignment(
+      phylogeny = ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);"),
+      alignment_params = create_alignment_params()
+    )
+  )
+})
+
+test_that("inout is checked", {
+  phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
+  alignment_params <- create_alignment_params()
+
+  expect_error(
+    sim_true_alignment(
+      phylogeny = "nonsense",
+      alignment_params = alignment_params
+    )
+  )
+  expect_error(
+    sim_true_alignment(
+      phylogeny = phylogeny,
+      alignment_params = "nonsense"
+    )
+  )
+})
+
+test_that("use linked_node_sub", {
+  phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
+  alignment_params <- create_alignment_params(
+    site_model = "linked_node_sub"
+  )
+  alignment <- sim_true_alignment(
+    phylogeny = phylogeny,
+    alignment_params = alignment_params,
+  )
+  expect_equal(nrow(alignment), ape::Ntip(phylogeny))
+  expect_equal(ncol(alignment), nchar(alignment_params$root_sequence))
+  # More detailed test are in
+  # test-sim_true_alignment_with_linked_node_sub_site_model.R
+})
+
+test_that("use unlinked_node_sub", {
+  phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
+  alignment_params <- create_alignment_params(
+    site_model = "unlinked_node_sub"
+  )
+  alignment <- sim_true_alignment(
+    phylogeny = phylogeny,
+    alignment_params = alignment_params,
+  )
+  expect_equal(nrow(alignment), ape::Ntip(phylogeny))
+  expect_equal(ncol(alignment), nchar(alignment_params$root_sequence))
+  # More detailed test are in
+  # test-sim_true_alignment_with_unlinked_node_sub_site_model.R
+})
+
