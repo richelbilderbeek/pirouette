493cefc7200783443d10f1194102f61757a6e041
G. Laudanno <P274829@workspace.rug.nl>
2019-10-31 15:33:29 +0100

    small changes

--- R/check_root_sequence.R
+++ R/check_root_sequence.R
@@ -1,6 +1,7 @@
 #' Check if the root sequence is valid
 #'
 #' Will \link{stop} if not
+#' @inheritParams default_params_doc
 #' @export
 check_root_sequence <- function(root_sequence) {
   if (!is_dna_seq(root_sequence) || length(root_sequence) != 1) {
