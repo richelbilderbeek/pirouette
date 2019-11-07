b14819ce84250f88e80374be4252165deb12144c
G. Laudanno <P274829@workspace.rug.nl>
2019-11-07 15:27:29 +0100

    added check_model_types

---
+++ R/check_model_type.R
@@ -0,0 +1,31 @@
+#' Checks if model type is valid
+#' Will \link{stop} if not.
+#' @inheritParams default_params_doc
+#' @return nothing. Will \link{stop} if not
+#' @author Giovanni Laudanno, Richèl J.C. Bilderbeek
+#' @examples
+#' library(testthat)
+#'
+#' expect_silent(check_model_type(get_model_types()[1]))
+#' expect_silent(check_model_type(get_model_types()[2]))
+#' expect_error(check_model_type("nonsense"))
+#' expect_error(check_model_type(NA))
+#' expect_error(check_model_type(NULL))
+#' @export
+check_model_type <- function(
+  model_type
+) {
+  model_types <- get_model_types()
+  out <- rep(NA, length(model_types))
+  for (l in seq_along(model_types)) {
+    out[l] <- paste("'", model_types[l], "'", sep = "")
+  }
+  if (!(model_type %in% model_types)) {
+    stop(
+      paste0(
+        "'model_type' must be among the following: ",
+        paste(out, collapse = ", "), "."
+      )
+    )
+  }
+}
