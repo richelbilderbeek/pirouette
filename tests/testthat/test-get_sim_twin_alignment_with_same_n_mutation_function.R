1b1b43b6753693fc5a43dabf5bec187bc0b720d5
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-13 12:58:09 +0100

    Update test

--- tests/testthat/test-get_sim_twin_alignment_with_same_n_mutation_function.R
+++ tests/testthat/test-get_sim_twin_alignment_with_same_n_mutation_function.R
@@ -1,9 +1,16 @@
 test_that("use", {
-  skip("WIP")
-
   f <- get_sim_twin_alignment_with_same_n_mutation_function()
   check_sim_twin_alignment_function(f)
 
-  alignment <- f(twin_phylogeny = ape::rcoal(3), true_alignment = "irrelevant")
-  testthat::expect_silent(pirouette::check_alignment(alignment))
+  true_alignment <- get_test_alignment()
+  root_sequence <- create_blocked_dna(
+    get_alignment_sequence_length(true_alignment)
+  )
+
+  alignment <- f(
+    twin_phylogeny = ape::rcoal(3),
+    true_alignment = true_alignment,
+    root_sequence = root_sequence
+  )
+  expect_silent(pirouette::check_alignment(alignment))
 })
