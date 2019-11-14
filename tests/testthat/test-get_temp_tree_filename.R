938557ab519fc0d578d4867a434832038712e45d
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-14 11:33:55 +0100

    Let test for X test X

--- tests/testthat/test-get_temp_tree_filename.R
+++ tests/testthat/test-get_temp_tree_filename.R
@@ -2,7 +2,7 @@ test_that("use", {
   expect_true(
     !is.na(
       stringr::str_match(
-        string = get_temp_fasta_filename(),
+        string = get_temp_tree_filename(),
         "tree_.*\\.newick"
       )[1, 1]
     )
