e36bb5eacce09f8d73f81ca069d396c6830306b6
richelbilderbeek <richel@richelbilderbeek.nl>
2019-12-19 14:03:40 +0100

    Export 'check_beast2_installed'

--- R/check_beast2_installed.R
+++ R/check_beast2_installed.R
@@ -1,6 +1,7 @@
 #' Checks if BEAST2 is installed
 #'
 #' Will \link{stop} if not.
+#' @export
 check_beast2_installed <- function() {
   if (!beastier::is_beast2_installed()) {
     stop("BEAST2 not installed. Tip: use 'beastier::install_beast2()'")
