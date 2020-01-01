c0e8c4d01c83cfb1e906ea858f6eef004d4c6714
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-01 11:43:15 +0100

    Improve test of 'check_beast2_installed'

--- tests/testthat/test-check_beast2_installed.R
+++ tests/testthat/test-check_beast2_installed.R
@@ -1,11 +1,21 @@
 test_that("use", {
 
-  if (beastier::is_beast2_installed()) {
+  # If the user has no access to Internet,
+  # BEAST2 will be uninstalled without a way to
+  # reinstall. Therefore, only do this on a CI service
+  if (!is_on_ci()) return()
+
+  if (is_beast2_installed()) {
     expect_silent(check_beast2_installed())
-  } else {
-    expect_error(
-      check_beast2_installed(),
-      "BEAST2 not installed. Tip: use 'beastier::install_beast2()'"
-    )
+    uninstall_beast2()
+    expect_error(check_beast2_installed())
+    install_beast2()
+    expect_silent(check_beast2_installed())
+  } else  {
+    expect_error(check_beast2_installed())
+    install_beast2()
+    expect_silent(check_beast2_installed())
+    uninstall_beast2()
+    expect_error(check_beast2_installed())
   }
 })
