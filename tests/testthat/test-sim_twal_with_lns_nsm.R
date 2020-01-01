e3a1237b6bf0d402d4989467908811aa62fa4228
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-01 11:34:50 +0100

    Remove duplicate 'sim_twal_with_std_nsm_raw' (duplicate of 'sim_twal_with_std_nsm')

---
+++ tests/testthat/test-sim_twal_with_lns_nsm.R
@@ -0,0 +1,7 @@
+test_that("use", {
+  alignment <- sim_twal_with_lns_nsm(
+    twin_phylogeny = ape::read.tree(text = "((A:1, B:1):1, C:2);"),
+    root_sequence = "aaaa"
+  )
+  expect_silent(check_alignment(alignment))
+})
