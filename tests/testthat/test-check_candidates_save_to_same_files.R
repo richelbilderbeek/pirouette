652223633d8281020c469d08122555954b76cb30
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-01 12:10:51 +0100

    Test 'check_candidates_save_to_same_files'

---
+++ tests/testthat/test-check_candidates_save_to_same_files.R
@@ -0,0 +1,79 @@
+test_that("use", {
+
+  experiments <- list()
+  experiments[[1]] <- create_test_cand_experiment()
+  experiments[[2]] <- experiments[[1]]
+
+  expect_silent(check_candidates_save_to_same_files(experiments))
+})
+
+test_that("first tracelog filename is NA", {
+
+  experiments <- list()
+  experiments[[1]] <- create_test_cand_experiment()
+  experiments[[2]] <- experiments[[1]]
+  experiments[[1]]$inference_model$mcmc$tracelog$filename <- NA
+
+  expect_error(check_candidates_save_to_same_files(experiments))
+})
+
+test_that("tracelog filenames differ", {
+
+  experiments <- list()
+  experiments[[1]] <- create_test_cand_experiment()
+  experiments[[2]] <- experiments[[1]]
+  experiments[[1]]$inference_model$mcmc$tracelog$filename <- "different.log"
+
+  expect_error(check_candidates_save_to_same_files(experiments))
+})
+
+test_that("first screenlog filename is NA", {
+
+  experiments <- list()
+  experiments[[1]] <- create_test_cand_experiment()
+  experiments[[2]] <- experiments[[1]]
+  experiments[[1]]$inference_model$mcmc$screenlog$filename <- NA
+
+  expect_error(check_candidates_save_to_same_files(experiments))
+})
+
+test_that("screenlog filenames differ", {
+
+  experiments <- list()
+  experiments[[1]] <- create_test_cand_experiment()
+  experiments[[2]] <- experiments[[1]]
+  experiments[[1]]$inference_model$mcmc$screenlog$filename <- "different.csv"
+
+  expect_error(check_candidates_save_to_same_files(experiments))
+})
+
+
+test_that("first treelog filename is NA", {
+
+  experiments <- list()
+  experiments[[1]] <- create_test_cand_experiment()
+  experiments[[2]] <- experiments[[1]]
+  experiments[[1]]$inference_model$mcmc$treelog$filename <- NA
+
+  expect_error(check_candidates_save_to_same_files(experiments))
+})
+
+test_that("treelog filenames differ", {
+
+  experiments <- list()
+  experiments[[1]] <- create_test_cand_experiment()
+  experiments[[2]] <- experiments[[1]]
+  experiments[[1]]$inference_model$mcmc$treelog$filename <- "different.trees"
+
+  expect_error(check_candidates_save_to_same_files(experiments))
+})
+
+test_that("treelog filenames differ", {
+
+  experiments <- list()
+  experiments[[1]] <- create_test_cand_experiment()
+  experiments[[2]] <- experiments[[1]]
+  experiments[[1]]$errors_filename <- "different.csv"
+
+  expect_error(check_candidates_save_to_same_files(experiments))
+})
