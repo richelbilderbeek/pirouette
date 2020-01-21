3a626187a59deca6d88fb925b60e11c607086a8a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-21 11:38:22 +0100

    Examples 9 and 10 work again, progress #377

--- tests/testthat/test-pir_plot_from_file.R
+++ tests/testthat/test-pir_plot_from_file.R
@@ -50,8 +50,6 @@ test_that("use, generative + twinning", {
 
   if (!beastier::is_on_travis()) return()
 
-  skip("Issue 377, Issue #377")
-
   pir_out_filename <- tempfile(fileext = ".csv")
   url <- "https://raw.githubusercontent.com/richelbilderbeek/pirouette_example_9/master/example_9_314/errors.csv" # nolint indeed a long URL
   utils::download.file(url = url, destfile = pir_out_filename, quiet = TRUE)
@@ -64,8 +62,6 @@ test_that("use, generative + twinning", {
 test_that("use, best_candidate + twinning", {
 
   if (!beastier::is_on_travis()) return()
-
-  skip("Issue 377, Issue #377")
 
   pir_out_filename <- tempfile(fileext = ".csv")
   url <- "https://raw.githubusercontent.com/richelbilderbeek/pirouette_example_10/master/example_10_314/errors.csv" # nolint indeed a long URL
