d0889926a2ee74e4f6e86ddf5688dad7d95a8a60
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-22 13:48:21 +0100

    Fix doc

--- R/pir_runs.R
+++ R/pir_runs.R
@@ -6,6 +6,8 @@
 #' @inheritParams default_params_doc
 #' @return a list of \link{pir_run} outputs.
 #' @examples
+#' library(testthat)
+#'
 #' if (is_on_travis() && is_beast2_installed()) {
 #'
 #'   pir_paramses <- list()
