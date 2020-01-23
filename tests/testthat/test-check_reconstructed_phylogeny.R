2eb42b1c69dec3f95225373d0fff07d4264cdd36
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-23 09:47:29 +0100

    Tests have no RNG

--- tests/testthat/test-check_reconstructed_phylogeny.R
+++ tests/testthat/test-check_reconstructed_phylogeny.R
@@ -1,14 +1,29 @@
 test_that("use", {
 
-  expect_silent(check_reconstructed_phylogeny(ape::rcoal(4)))
+  #
+  #     +-------------- C
+  # ----+
+  #     |    +--------- B
+  #     +----+
+  #          +--------- A
+  #
+  expect_silent(
+    check_reconstructed_phylogeny(
+      ape::read.tree(text = "((A:2, B:2):1, C:3);")
+    )
+  )
 
-  set.seed(42)
-  p_with_extant <- ape::rtree(5)
-  testit::assert(geiger::is.extinct(p_with_extant))
-  #ape::plot.phylo(p_with_extant)
-  #ape::plot.phylo(ape::drop.fossil(p_with_extant))
+  #
+  #     +-------------- C
+  # ----+
+  #     |    +--------- B
+  #     +----+
+  #          +---- A
+  #
   expect_error(
-    check_reconstructed_phylogeny(p_with_extant),
+    check_reconstructed_phylogeny(
+      ape::read.tree(text = "((A:1, B:2):1, C:3);")
+    ),
     "A reconstructed phylogeny must not contain extinct species"
   )
 })
