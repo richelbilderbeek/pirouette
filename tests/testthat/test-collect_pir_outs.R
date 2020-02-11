abaa4d91a8109269f11dceb1b32aeb6359107f5d
Giappo <glaudanno@gmail.com>
2020-02-11 18:39:27 +0100

    simpler test for collect_pir_outs

--- tests/testthat/test-collect_pir_outs.R
+++ tests/testthat/test-collect_pir_outs.R
@@ -1,49 +1,25 @@
 test_that("minimal runs", {
 
-  if (!beastier::is_on_travis()) return()
   if (!beastier::is_beast2_installed()) return()
 
-  phylogenies <- list()
-  phylogenies[[1]] <- ape::read.tree(text = "((A:2, B:2):1, C:3);")
-  phylogenies[[2]] <- ape::read.tree(text = "((A:1, B:1):2, C:3);")
-
-  # experiment
-  chain_lenght <- 1e4
-  store_every <- 1e3
-  experiment <- pirouette::create_gen_experiment()
-  experiment$inference_model$mcmc$chain_length <- chain_length
-  experiment$inference_model$mcmc$store_every <- store_every
-  experiment$est_evidence_mcmc$chain_length <- chain_length
-  experiment$est_evidence_mcmc$store_every <- store_every
-  experiments <- list(experiment)
-
-  # twinning params
-  twinning_params <- pirouette::create_twinning_params(
-    sim_twin_tree_fun = pirouette::get_sim_bd_twin_tree_fun(),
-    sim_twal_fun = pirouette::get_sim_twal_with_std_nsm_fun(
-      mutation_rate = pirouette::create_standard_mutation_rate(
-        phylogeny = phylogenies[[1]]
-      ),
-      site_model = beautier::create_jc69_site_model()
-    )
+  pir_outs <- list()
+  pir_outs[[1]] <- create_test_pir_run_output(
+    add_twin = TRUE,
+    add_best = TRUE
   )
-
-  pir_paramses <- list()
-  pir_paramses[[2]] <- pir_paramses[[1]] <-
-    pirouette::create_test_pir_params(
-      twinning_params = twinning_params,
-      experiments = experiments
-    )
-
-  pir_outs <- pirouette::pir_runs(
-    phylogenies = phylogenies,
-    pir_paramses = pir_paramses
+  pir_outs[[2]] <- create_test_pir_run_output(
+    add_twin = TRUE,
+    add_best = TRUE
   )
-  testit::assert(length(pir_outs) == length(phylogenies))
+  pir_outs[[2]]$error_1 <- 2 * pir_outs[[1]]$error_3
+  pir_outs[[2]]$error_2 <- 2 * pir_outs[[1]]$error_1
+  pir_outs[[2]]$error_3 <- 2 * pir_outs[[1]]$error_2
 
   pir_out <- pirouette::collect_pir_outs(pir_outs)
+  testthat::expect_true(nrow(pir_out) == 4)
   testthat::expect_true(
-    sum(grepl(pattern = "error_", x = colnames(pir_out))) ==
-      length(phylogenies) * (chain_lenght / store_every)
+    sum(grepl(x = colnames(pir_out), "error_")) ==
+      sum(grepl(x = colnames(pir_outs[[1]]), "error_")) +
+      sum(grepl(x = colnames(pir_outs[[2]]), "error_"))
   )
 })
