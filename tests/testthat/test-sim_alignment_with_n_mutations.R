8a6df23c272495f9dce81c29178c19d5b5b48767
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-13 08:48:38 +0100

    Check verbosity

--- tests/testthat/test-sim_alignment_with_n_mutations.R
+++ tests/testthat/test-sim_alignment_with_n_mutations.R
@@ -2,8 +2,8 @@ test_that("minimal use", {
 
   # Beware: the 'n_mutations = 9' is due to the RNG always ending up
   # at 9 mutations.
-  testthat::expect_silent(
-    pirouette::sim_alignment_with_n_mutations(
+  expect_silent(
+    sim_alignment_with_n_mutations(
       phylogeny = ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);"),
       root_sequence = "acgt",
       n_mutations = 9
@@ -11,37 +11,48 @@ test_that("minimal use", {
   )
 })
 
+test_that("check verbosity", {
+
+  expect_output(
+    sim_alignment_with_n_mutations(
+      phylogeny = ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);"),
+      root_sequence = "acgt",
+      n_mutations = 9,
+      verbose = TRUE
+    )
+  )
+})
 test_that("input is checked", {
   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
-  alignment_params <- pirouette::create_alignment_params()
+  alignment_params <- create_alignment_params()
   n_mutations <- 9
 
-  testthat::expect_error(
-    pirouette::sim_alignment_with_n_mutations(
+  expect_error(
+    sim_alignment_with_n_mutations(
       phylogeny = "nonsense",
       root_sequence = "acgt",
       n_mutations = n_mutations
     ),
     "phylogeny.*phylogeny"
   )
-  testthat::expect_error(
-    pirouette::sim_alignment_with_n_mutations(
+  expect_error(
+    sim_alignment_with_n_mutations(
       phylogeny = phylogeny,
       root_sequence = "nonsense",
       n_mutations = n_mutations
     ),
     "root_sequence.*DNA"
   )
-  testthat::expect_error(
-    pirouette::sim_alignment_with_n_mutations(
+  expect_error(
+    sim_alignment_with_n_mutations(
       phylogeny = phylogeny,
       root_sequence = "acgt",
       n_mutations = "nonsense"
     ),
     "beautier::is_one_int.n_mutations. is not TRUE"
   )
-  testthat::expect_error(
-    pirouette::sim_alignment_with_n_mutations(
+  expect_error(
+    sim_alignment_with_n_mutations(
       phylogeny = phylogeny,
       root_sequence = "acgt",
       n_mutations = 1e123,
@@ -53,35 +64,35 @@ test_that("input is checked", {
 
 test_that("use linked_node_sub (lns)", {
   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
-  alignment_params <- pirouette::create_test_alignment_params(
+  alignment_params <- create_test_alignment_params(
     sim_tral_fun =
-      pirouette::get_sim_tral_with_lns_nsm_fun()
+      get_sim_tral_with_lns_nsm_fun()
   )
   # 'n_mutations' is set to the first number of generated mutations
   set.seed(42)
-  alignment <- pirouette::sim_alignment_with_n_mutations(
+  alignment <- sim_alignment_with_n_mutations(
     phylogeny = phylogeny,
     root_sequence = "acgt",
     n_mutations = 11,
     verbose = FALSE
   )
-  testthat::expect_equal(nrow(alignment), ape::Ntip(phylogeny))
-  testthat::expect_equal(ncol(alignment), nchar(alignment_params$root_sequence))
+  expect_equal(nrow(alignment), ape::Ntip(phylogeny))
+  expect_equal(ncol(alignment), nchar(alignment_params$root_sequence))
 })
 
 test_that("use unlinked_node_sub (uns)", {
   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
-  alignment_params <- pirouette::create_test_alignment_params(
+  alignment_params <- create_test_alignment_params(
     sim_tral_fun =
-      pirouette::get_sim_tral_with_uns_nsm_fun()
+      get_sim_tral_with_uns_nsm_fun()
   )
   # Beware: the 'n_mutations = 9' is due to the RNG always ending up
   # at 9 mutations.
-  alignment <- pirouette::sim_alignment_with_n_mutations(
+  alignment <- sim_alignment_with_n_mutations(
     phylogeny = phylogeny,
     root_sequence = "acgt",
     n_mutations = 9
   )
-  testthat::expect_equal(nrow(alignment), ape::Ntip(phylogeny))
-  testthat::expect_equal(ncol(alignment), nchar(alignment_params$root_sequence))
+  expect_equal(nrow(alignment), ape::Ntip(phylogeny))
+  expect_equal(ncol(alignment), nchar(alignment_params$root_sequence))
 })
