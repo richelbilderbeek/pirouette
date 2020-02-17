2bf2720b55e05dde67e94c72b0250292261860b0
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-17 08:32:14 +0100

    Expose #389, @Giappo will enjoy this

---
+++ R/pir_run_distribution.R
@@ -0,0 +1,12 @@
+#' Do multiple \link{pirouette} runs off a distribition of phylogenies
+#'
+#' This is a simple convenience functions: supply a phylogeny generator
+#' function and one or more  \link{pirouette} parameter sets.
+#' For each parameters pair, \link{pir_run} is called with a phylogeny
+#' drawn from the phylogeny generator function.
+#' @inheritParams default_params_doc
+#' @return a list of \link{pir_run} outputs.
+#' @export
+pir_run_distribution <- function() {
+
+}
