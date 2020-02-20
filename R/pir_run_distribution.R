ca94e961efa3b6082554610605b8849688e3afdb
Giappo <glaudanno@gmail.com>
2020-02-20 19:43:02 +0100

    pir_run_distribution

--- R/pir_run_distribution.R
+++ R/pir_run_distribution.R
@@ -11,5 +11,41 @@ pir_run_distribution <- function(
   sim_phylo_fun,
   pir_paramses
 ) {
+  # checks
+  if (!is.list(pir_paramses)) {
+    stop("'pir_paramses' must be a list of pir_params objects")
+  }
+  if (length(pir_paramses) == 0) {
+    stop("'pir_paramses' must be a list of pir_params objects")
+  }
+  for (i in seq_along(pir_paramses)) {
+    tryCatch(
+      pirouette::check_pir_params(pir_paramses[[i]]),
+      error = function(e) {
+        stop("'pir_paramses' must be a list of pir_params objects")
+      }
+    )
+  }
+  if (!is.function(sim_phylo_fun)) {
+    stop("'sim_phylo_fun' must be a function")
+  }
+  if (!beautier::is_phylo(sim_phylo_fun())) {
+    stop("'sim_phylo_fun' must be a function that returns an ape::phylo object")
+  }
 
+  # simulate trees
+  pir_outs <- l_parses <- length(pir_paramses)
+  phylogenies <- vector("list", l_parses)
+  for (seed in seq_along(pir_paramses)) {
+    set.seed(seed)
+    phylogenies[[seed]] <- sim_phylo_fun()
+  }
+
+  # pir run!
+  pir_outs <- pirouette::pir_runs(
+    phylogenies = phylogenies,
+    pir_paramses = pir_paramses
+  )
+
+  pir_outs
 }
