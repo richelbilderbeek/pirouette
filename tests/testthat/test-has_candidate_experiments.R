628685ef5e9ca513bb47518d16b6b4bf07a130e7
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-26 12:38:24 +0100

    Increase use of 'create_test_pir_params_setup'

--- tests/testthat/test-has_candidate_experiments.R
+++ tests/testthat/test-has_candidate_experiments.R
@@ -1,36 +1,27 @@
 test_that("minimal use", {
-  pir_params <- pirouette::create_test_pir_params()
+  pir_params <- create_test_pir_params()
   testit::assert(length(pir_params$experiments) == 1)
   testit::assert(
     pir_params$experiments[[1]]$inference_conditions$model_type ==
       "generative"
   )
-  testthat::expect_false(pirouette::has_candidate_experiments(pir_params))
+  expect_false(has_candidate_experiments(pir_params))
 })
 
-test_that("one candidate model", {
+test_that("gen", {
 
-  if (rappdirs::app_dir()$os == "win") return()
-
-  pir_params <- pirouette::create_test_pir_params(
-    experiments = list(pirouette::create_test_cand_experiment())
+  pir_params <- create_test_pir_params_setup(
+    has_candidate = FALSE
   )
-  testthat::expect_true(pirouette::has_candidate_experiments(pir_params))
+  expect_true(!has_candidate_experiments(pir_params))
 })
 
 test_that("gen + cand", {
 
   if (rappdirs::app_dir()$os == "win") return()
 
-  pir_params <- create_test_pir_params(
-    experiments = list(
-      pirouette::create_test_gen_experiment(),
-      pirouette::create_test_cand_experiment(
-        inference_model = beautier::create_test_inference_model(
-          site_model = beautier::create_gtr_site_model()
-        )
-      )
-    )
+  pir_params <- create_test_pir_params_setup(
+    has_candidate = TRUE
   )
-  testthat::expect_true(has_candidate_experiments(pir_params))
+  expect_true(has_candidate_experiments(pir_params))
 })
