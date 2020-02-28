17921747242cf767c0cd2de8b4c42313b0249cf6
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 14:30:37 +0100

    Add 'pir_save'

---
+++ R/pir_save.R
@@ -0,0 +1,34 @@
+#' Save all output from \link{pir_run}
+#' @inheritParams default_params_doc
+#' @export
+pir_save <- function(
+  phylogeny,
+  pir_out,
+  folder_name
+) {
+  ape::write.tree(
+    phylogeny,
+    file = file.path(folder_name, "phylogeny.newick")
+  )
+
+  utils::write.csv(
+    x = pir_out,
+    file = file.path(folder_name, "errors.csv"),
+    row.names = FALSE
+  )
+
+  pir_plot(pir_out) +
+    ggplot2::ggsave(file.path(folder_name, "errors.png"))
+
+  pir_to_pics(
+    phylogeny = phylogeny,
+    pir_params = pir_params,
+    folder = folder_name
+  )
+
+  pir_to_tables(
+    pir_params = pir_params,
+    folder = folder_name
+  )
+
+}
