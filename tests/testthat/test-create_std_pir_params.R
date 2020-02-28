a29f207772a2c4026d7d3018f3b4e53856f474be
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 14:05:28 +0100

    Check Windows control flow

--- tests/testthat/test-create_std_pir_params.R
+++ tests/testthat/test-create_std_pir_params.R
@@ -1,4 +1,8 @@
 test_that("use", {
+  expect_silent(check_pir_params(create_std_pir_params(os = "win")))
+})
+
+test_that("use", {
   if (rappdirs::app_dir()$os == "win") return()
   expect_silent(check_pir_params(create_std_pir_params()))
 })
