70b2cfd56362ea45b8b36c2a4bfad3069e8fb2fb
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 15:15:15 +0100

    Fix test

--- tests/testthat/test-shorten_pir_paramses.R
+++ tests/testthat/test-shorten_pir_paramses.R
@@ -5,14 +5,14 @@ test_that("use", {
   pir_paramses <- create_std_pir_paramses(n = 2)
 
   for (pir_params in pir_paramses) {
-    for (experiment in pir_params$experiments)) {
+    for (experiment in pir_params$experiments) {
       expect_false(experiment$inference_model$mcmc$chain_length == 3000)
       expect_false(experiment$est_evidence_mcmc$chain_length == 3000)
     }
   }
   pir_paramses <- shorten_pir_paramses(pir_paramses)
   for (pir_params in pir_paramses) {
-    for (experiment in pir_params$experiments)) {
+    for (experiment in pir_params$experiments) {
       expect_true(experiment$inference_model$mcmc$chain_length == 3000)
       expect_true(experiment$est_evidence_mcmc$chain_length == 3000)
     }
