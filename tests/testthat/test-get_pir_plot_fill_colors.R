52be300952342dea7a4f54558b5a93234f024df2
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-13 19:02:32 +0200

    Check color mapping

--- tests/testthat/test-get_pir_plot_fill_colors.R
+++ tests/testthat/test-get_pir_plot_fill_colors.R
@@ -1,5 +1,11 @@
 test_that("c", {
   fill_colors <- get_pir_plot_fill_colors()
   expect_silent(get_pir_plot_fill_colors())
+
+  expect_equal(names(fill_colors), get_tree_and_model_values())
+  expect_equal(as.character(fill_colors["true_generative"]), "#FF3333") # Red
+  expect_equal(as.character(fill_colors["twin_generative"]), "#F99F55") # Orange
+  expect_equal(as.character(fill_colors["true_candidate"]), "#3333FF") # Blue
+  expect_equal(as.character(fill_colors["twin_candidate"]), "#559988") # Green
 })
 
--- tests/testthat/test-get_pir_plot_line_colors.R
+++ tests/testthat/test-get_pir_plot_line_colors.R
@@ -1,5 +1,11 @@
 test_that("c", {
   line_colors <- get_pir_plot_line_colors()
   expect_silent(get_pir_plot_line_colors())
+
+  expect_equal(names(line_colors), get_tree_and_model_values())
+  expect_equal(as.character(line_colors["true_generative"]), "#FF0000") # Red
+  expect_equal(as.character(line_colors["twin_generative"]), "#E77E22") # Orange
+  expect_equal(as.character(line_colors["true_candidate"]), "#0000FF") # Blue
+  expect_equal(as.character(line_colors["twin_candidate"]), "#229955") # Green
 })
 
