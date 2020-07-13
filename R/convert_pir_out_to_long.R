07cc07560a6455fcdb13a314140afbfd3170985a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-13 16:56:54 +0200

    Simplify 'convert_pir_out_to_long'

--- R/convert_pir_out_to_long.R
+++ R/convert_pir_out_to_long.R
@@ -15,65 +15,31 @@ convert_pir_out_to_long <- function(
 ) {
   pirouette::check_pir_out(pir_out)
 
-  ##### Data wrangling #####
-  # Convert to long form
   df <- pir_out
+  df <- dplyr::rename(df, tree_and_model = tree)
+  df$tree_and_model <- interaction(
+    df$tree_and_model,
+    df$inference_model,
+    sep = "_"
+  )
+  df$inference_model <- NULL
+  df$inference_model_weight <- NULL
+  df$site_model <- NULL
+  df$clock_model <- NULL
+  df$tree_prior <- NULL
+  df
+
   first_col_index <- which(names(df) == "error_1")
   testthat::expect_equal(1, length(first_col_index))
 
   df_long <- tidyr::gather(
     df, "error_index", "error_value", first_col_index:ncol(df)
   )
-  testthat::expect_true("error_index" %in% names(df_long))
-  df_long$error_index <- NULL
   testthat::expect_true("error_value" %in% names(df_long))
 
-  # Convert factor values to human-readable strings
-  df_long$site_model <- plyr::revalue(
-    df_long$site_model, c("JC69" = "JC", "TN93" = "TN"), warn_missing = FALSE)
-  df_long$clock_model <- plyr::revalue(
-    df_long$clock_model,
-    c("strict" = "Strict", "relaxed_log_normal" = "RLN"), warn_missing = FALSE
-  )
-  df_long$tree_prior <- plyr::revalue(
-    df_long$tree_prior,
-    c(
-      "yule" = "Yule",
-      "birth_death" = "BD",
-      "coalescent_bayesian_skyline" = "CBS",
-      "coalescent_constant_population" = "CCP",
-      "coalescent_exp_population" = "CEP"
-    ),
-    warn_missing = FALSE
-  )
-
-  # Add column tree_and_model, the combination of tree and model type
-  df_long$tree_and_model <- interaction(
-    df_long$tree,
-    df_long$inference_model,
-    sep = "_"
-  )
-
-  # Add model_setting, the combination of all inference models
-  df_long$model_setting <- interaction(
-    df_long$site_model,
-    df_long$clock_model,
-    df_long$tree_prior,
-    sep = ", "
-  )
-  df_long <- df_long[order(df_long$tree), ]
-  df_long$model_setting <-
-    factor(df_long$model_setting, levels = unique(df_long$model_setting))
-
-  # Remove useless columns
-  df_long$inference_model <- NULL
-  df_long$tree <- NULL
-  df_long$inference_model_weight <- NULL
-  df_long$site_model <- NULL
-  df_long$clock_model <- NULL
-  df_long$tree_prior <- NULL
-  df_long$model_setting <- NULL
-
+  # 'error_index' is added by gather, remove it
+  testthat::expect_true("error_index" %in% names(df_long))
+  df_long$error_index <- NULL
 
   rownames(df_long) <- mapply(seq_len(nrow(df_long)), FUN = toString)
 
