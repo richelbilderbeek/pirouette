691c802526b11a6cd5ab6921760f574da47b04a6
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-14 14:26:46 +0200

    Check that values from files are correctly classified, fix #423

--- tests/testthat/test-create_tree_and_model_errors_from_folders.R
+++ tests/testthat/test-create_tree_and_model_errors_from_folders.R
@@ -70,14 +70,3 @@ test_that("values from files are correctly placed", {
     tolerance = 0.01
   )
 })
-
-test_that("use", {
-  skip("WIP")
-  super_folder <- "/home/richel/pirouette_example_32/pirouette_example_32/example_32" # nolint long line indeed
-  folder_names <- list.dirs(super_folder)
-  folder_names <- folder_names[folder_names != super_folder]
-  tree_and_model_errors <- create_tree_and_model_errors_from_folders(
-    folder_names
-  )
-  pirouette::check_tree_and_model_errors(tree_and_model_errors)
-})
