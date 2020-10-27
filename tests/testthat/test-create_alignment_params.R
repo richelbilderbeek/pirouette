41ba88ceb6d636e6955ec84421abbfb35cc29c16
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-27 08:16:14 +0100

    nodeSub is not on CRAN yet

--- tests/testthat/test-create_alignment_params.R
+++ tests/testthat/test-create_alignment_params.R
@@ -11,12 +11,12 @@ test_that("use", {
 test_that("sim_tral_fun", {
 
   # nodeSub is not on CRAN yet
-  # expect_silent(
-  #   pirouette::create_alignment_params(
-  #     sim_tral_fun =
-  #       pirouette::get_sim_tral_with_lns_nsm_fun()
-  #   )
-  # )
+  # expect_silent( # nolint
+  #   pirouette::create_alignment_params( # nolint
+  #     sim_tral_fun = # nolint
+  #       pirouette::get_sim_tral_with_lns_nsm_fun() # nolint
+  #   ) # nolint
+  # ) # nolint
 
   expect_silent(
     pirouette::create_alignment_params(
--- tests/testthat/test-sim_true_alignment.R
+++ tests/testthat/test-sim_true_alignment.R
@@ -31,18 +31,18 @@ test_that("inout is checked", {
 #
 # test_that("use linked_node_sub (lns)", {
 #
-#   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
-#   alignment_params <- pirouette::create_alignment_params(
-#     sim_tral_fun =
-#       pirouette::get_sim_tral_with_lns_nsm_fun(),
-#     root_sequence <- "aaccggtt"
-#   )
-#   alignment <- pirouette::sim_true_alignment(
-#     true_phylogeny = phylogeny,
-#     alignment_params = alignment_params,
-#   )
-#   expect_equal(nrow(alignment), ape::Ntip(phylogeny))
-#   expect_equal(ncol(alignment), nchar(root_sequence))
+#   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);") # nolint
+#   alignment_params <- pirouette::create_alignment_params( # nolint
+#     sim_tral_fun = # nolint
+#       pirouette::get_sim_tral_with_lns_nsm_fun(), # nolint
+#     root_sequence <- "aaccggtt" # nolint
+#   ) # nolint
+#   alignment <- pirouette::sim_true_alignment( # nolint
+#     true_phylogeny = phylogeny, # nolint
+#     alignment_params = alignment_params, # nolint
+#   ) # nolint
+#   expect_equal(nrow(alignment), ape::Ntip(phylogeny)) # nolint
+#   expect_equal(ncol(alignment), nchar(root_sequence)) # nolint
 #   # More detailed test are in
 #   # test-sim_tral_with_lns_nsm.R
 # })
@@ -51,19 +51,19 @@ test_that("inout is checked", {
 #
 # test_that("use unlinked_node_sub (uns)", {
 #
-#   root_sequence <- "aaaa"
-#   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);")
-#   alignment_params <- pirouette::create_alignment_params(
-#     sim_tral_fun =
-#       pirouette::get_sim_tral_with_uns_nsm_fun(),
-#     root_sequence = root_sequence
-#   )
-#   alignment <- pirouette::sim_true_alignment(
-#     true_phylogeny = phylogeny,
-#     alignment_params = alignment_params,
-#   )
-#   expect_equal(nrow(alignment), ape::Ntip(phylogeny))
-#   expect_equal(ncol(alignment), nchar(root_sequence))
+#   root_sequence <- "aaaa" # nolint
+#   phylogeny <- ape::read.tree(text = "(((A:1, B:1):1, C:2):1, D:3);") # nolint
+#   alignment_params <- pirouette::create_alignment_params( # nolint
+#     sim_tral_fun = # nolint
+#       pirouette::get_sim_tral_with_uns_nsm_fun(), # nolint
+#     root_sequence = root_sequence # nolint
+#   ) # nolint
+#   alignment <- pirouette::sim_true_alignment( # nolint
+#     true_phylogeny = phylogeny, # nolint
+#     alignment_params = alignment_params, # nolint
+#   ) # nolint
+#   expect_equal(nrow(alignment), ape::Ntip(phylogeny)) # nolint
+#   expect_equal(ncol(alignment), nchar(root_sequence)) # nolint
 #   # More detailed test are in
 #   # test-sim_tral_with_uns_nsm.R
 # })
