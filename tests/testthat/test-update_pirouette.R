42fb1aa72c6930007ea95338f942fadc01bf6e35
richelbilderbeek <richel@richelbilderbeek.nl>
2022-09-09 12:44:49 +0200

    Do not write to folders, progress #431

--- tests/testthat/test-update_pirouette.R
+++
@@ -1,5 +0,0 @@
-test_that("update in silence", {
-  if (!beautier::is_on_github_actions()) return()
-  update_pirouette(upgrade = "always")
-  expect_silent(update_pirouette(upgrade = "always"))
-})
